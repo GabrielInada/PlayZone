6dcb1e27372c4a88ef73b84d85ae592e7a9a148d
GabrielInada <gabrieljunichiro2@gmail.com>
2026-02-19 05:31:38 -0300

    Update app.module.ts

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -6,9 +6,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from 'src/config/configuration';
 import { Player } from '../player/entities/player.entity';
 import { PlayerModule } from '../player/player.module';
+import { TeamModule } from '../team/team.module';
+import { Team } from '../team/entities/team.entity';
+import { Match } from '../match/entities/match.entity';
+import { MatchModule } from '../match/match.module';
+import { User } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
+import { Card } from '../card/entities/card.entity';
+import { Goal } from '../goal/entities/goal.entity';
+import { MatchReport } from '../match-report/entities/match-report.entity';
+import { MatchReportModule } from '../match-report/match-report.module';
+import { CardModule } from '../card/card.module';
+import { GoalModule } from '../goal/goal.module';
+import { Club } from '../club/entities/club.entity';
+import { ClubModule } from '../club/club.module';
+import { SelfConsultModule } from 'src/tasks/self-consult/self-consult.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
+    SelfConsultModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
@@ -26,7 +44,7 @@ import { PlayerModule } from '../player/player.module';
         };
       },
     }),
-    PlayerModule,
+    PlayerModule, TeamModule, MatchModule, UserModule, MatchReportModule, GoalModule, CardModule, ClubModule,
   ],
   controllers: [AppController],
   providers: [AppService],
