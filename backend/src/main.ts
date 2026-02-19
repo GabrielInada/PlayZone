e3ff377919dd57fabaa4a063311d0cc4eb44fa2d
GabrielInada <gabrieljunichiro2@gmail.com>
2026-02-19 12:42:21 -0300

    add setup for swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import configuration from './config/configuration';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const config = configuration();
-  const port = Number(process.env.PORT ?? config.port ?? 3000);
+  const { port } = configuration();
   const logger = new Logger('NestApplication');
 
   const app = await NestFactory.create(AppModule);
@@ -16,10 +16,19 @@ async function bootstrap() {
     forbidNonWhitelisted: true, // Retorna erro se enviarem campos desconhecidos
     transform: true,            // Converte tipos (ex: string "1" vira number 1)
   }));
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Playzone API')
+    .setDescription('Documentação da API PlayZone')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(port);
-  
   logger.log(`Backend is alive on ${await app.getUrl()}`);
 }
 
-bootstrap();
+void bootstrap();
